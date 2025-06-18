784c9829da3f33c1b6750aa8a7bf2d38aae4c6e8
Abdullah Suhail <123826131+AbdullahS17053@users.noreply.github.com>
2025-06-18 22:55:48 +0500

    random changes

---
+++ OilDigger/Assets/Scripts/CameraController.cs
@@ -0,0 +1,38 @@
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+
+    public float panSpeed = 10f;
+
+    private Vector3 lastMousePosition;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+         if (Input.GetMouseButtonDown(0)) // Right mouse button pressed
+        {
+            Debug.Log("CameraController Update called");
+            lastMousePosition = Input.mousePosition;
+        }
+
+        if (Input.GetMouseButton(0)) // Right mouse button held
+        {
+            Vector3 delta = Input.mousePosition - lastMousePosition;
+
+            // Move along X (horizontal) and Z (vertical in world space)
+            Vector3 move = new Vector3(-delta.x, 0, -delta.y) * panSpeed * Time.deltaTime;
+
+            transform.Translate(move, Space.World);
+
+            lastMousePosition = Input.mousePosition;
+        }
+    }
+}
